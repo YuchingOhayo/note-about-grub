1e7bb03b271f7bcc51b322cdacf11d5c1cebe917
samwhelp <samwhelp@users.noreply.github.com>
2022-03-20 18:03:58 +0800

    fix

--- _demo/prototype/boot_iso/demo_41_custom/Archcraft/latest/iso/iso-to-usb.sh
+++ _demo/prototype/boot_iso/demo_41_custom/Archcraft/latest/iso/iso-to-usb.sh
@@ -5,6 +5,7 @@
 ## Please run `lsblk` to find `of=/dev/sdb`
 ##
 
+
 #sudo dd if=./archcraft-2022.02.02-x86_64.iso of=/dev/sdb bs=4M status=progress oflag=sync
 #sudo dd if=./archcraft-xfce-2021.11.30-x86_64.iso of=/dev/sdb bs=4M status=progress oflag=sync
 #sudo dd if=./archcraft-lite-2021.11.13-x86_64.iso of=/dev/sdb bs=4M status=progress oflag=sync
--- _demo/prototype/boot_iso/demo_41_custom/MaboxLinux/latest/iso/iso-to-usb.sh
+++ _demo/prototype/boot_iso/demo_41_custom/MaboxLinux/latest/iso/iso-to-usb.sh
@@ -1,3 +1,9 @@
-sudo dd if=./mabox-linux-22.02-Herbolth-220227-linux515.iso of=/dev/sdb bs=4M status=progress && sync
-# https: //maboxlinux.org/users-guide/download-and-installation/
-# Please run `lsblk` to find `of=/dev/sdb`
+#!/usr/bin/env bash
+
+##
+## https://wiki.archcraft.io/docs/boot-iso/boot-with-usb
+## Please run `lsblk` to find `of=/dev/sdb`
+##
+
+
+#sudo dd if=./mabox-linux-22.02-Herbolth-220227-linux515.iso of=/dev/sdb bs=4M status=progress && sync
