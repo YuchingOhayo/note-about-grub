3c5a1e21fe8b71d9a6890164e16d1edf8f8f42d0
samwhelp <samwhelp@users.noreply.github.com>
2022-03-20 17:32:08 +0800

    iso-to-usb

---
+++ _demo/prototype/boot_iso/demo_41_custom/MaboxLinux/latest/iso/iso-to-usb.sh
@@ -0,0 +1,3 @@
+sudo dd if=./mabox-linux-22.02-Herbolth-220227-linux515.iso of=/dev/sdb bs=4M status=progress && sync
+# https: //maboxlinux.org/users-guide/download-and-installation/
+# Please run `lsblk` to find `of=/dev/sdb`
