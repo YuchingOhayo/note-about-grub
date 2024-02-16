0b23080a3e8a6957e3d3f71c6314e0ebcde4c186
YuchingOhayo <160224399+YuchingOhayo@users.noreply.github.com>
2024-02-16 17:18:06 +0800

    Update download.sh

--- _demo/prototype/boot_iso/demo_41_custom/ArchLinux/latest/iso/download.sh
+++ _demo/prototype/boot_iso/demo_41_custom/ArchLinux/latest/iso/download.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-
+curl -fLo ~/Downloads/iso/archlinux/archlinux-x86_64.iso --create-dirs \
+	https://mirror.archlinux.tw/ArchLinux/iso/latest/archlinux-x86_64.iso
 
 wget -c -i download.txt
